6bf994fd5305b1e56d25b2175ad3666c3ea9cb6a
SamarV-121 <samarvispute121@gmail.com>
2018-02-16 21:42:40 +0530

    sprout-common: Build Camera HIDLs

--- sprout.mk
+++ sprout.mk
@@ -101,6 +101,13 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     tinymix
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    android.hardware.camera.provider@2.4-impl
+
 # FM Radio
 PRODUCT_PACKAGES += \
     FMRadioGoogle \
