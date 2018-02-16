1e55b2dc3e89bb7f51eb4b2ab461bc71050dd9b5
SamarV-121 <samarvispute121@gmail.com>
2018-02-16 21:38:51 +0530

    sprout-common: Build Sensor HIDL

--- sprout.mk
+++ sprout.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
